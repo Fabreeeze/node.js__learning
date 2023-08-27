9197167f82f2911f0debb3440f5907b78629350c
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-08-28 00:37:16 +0530

    third commit, started using express js

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const http = require('http')
 
-const routes = require('./routes');
+const express = require('express')
+
+// const routes = require('./routes')
 // here routes accepts an object as imported stuff
 
+const app= express();
 
-const server = http.createServer(routes.handler);
+app.use( (req,res,next) => {
+    console.log("inside middleware");
+    //this function acts like a middleware
+
+
+    // next()
+    //next passed in arguements is a fucntion which allows the enxt middleware to be executed
+
+    res.send("<h1>using express here </h1>");
+});
+
+const server = http.createServer(app);
 
 server.listen(4000);
