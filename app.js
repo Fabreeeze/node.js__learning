fa7fddf0ae6491a4c35e0435cc9e5928a9f1706f
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-08-29 23:54:42 +0530

    third commit, a bit moe of express, added body-parser

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const http = require('http')
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 // const routes = require('./routes')
 // here routes accepts an object as imported stuff
 
 const app= express();
 
-app.use( (req,res,next) => {
-    console.log("inside middleware");
-    //this function acts like a middleware
+app.use(bodyParser.urlencoded);
 
+app.use('/add-product', (req,res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use( (req,res,next) => {
+    // console.log("inside middleware");
+    //this function acts like a middleware
 
     // next()
     //next passed in arguements is a fucntion which allows the enxt middleware to be executed
@@ -18,6 +27,7 @@ app.use( (req,res,next) => {
     res.send("<h1>using express here </h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(4000);
+app.listen(4000);
+// const server = http.createServer(app);
+// server.listen(4000);
+// app.listen(4000) does same work as above two lines
