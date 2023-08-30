5a044ff0b21819c835ba7d70784c0c954dad4282
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-08-30 23:49:09 +0530

    commit four, restructured some files into a folder, added 404 page , added a bit of url filtering for products page

--- app.js
+++ app.js
@@ -3,30 +3,39 @@ const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 // const routes = require('./routes')
 // here routes accepts an object as imported stuff
 
 const app= express();
 
-app.use(bodyParser.urlencoded);
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product', (req,res,next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+//app.use() runs for all requests
+//app.post() triggers for only post requests
+//similarly app.get() woks for get request only
+
+app.use('/admin',adminRoutes);
+//here '/admin' filters the requests and only requsts goin to '/admin/.... ' are 
+//sent throught to this code block
+app.use(shopRoutes);
+// app.use( (req,res,next) => {
+//     // console.log("inside middleware");
+//     //this function acts like a middleware
+
+//     // next()
+//     //next passed in arguements is a fucntion which allows the enxt middleware to be executed
+
+//     res.send("<h1>using express here </h1>");
+// });
+
+app.use((req,res,next)=>{
+    //this code is executed when no other app.use() code thingies match and hence is a page not on
+    // our site
+    res.status(404).send('<h1>Page not found!!! </h1>');
 });
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use( (req,res,next) => {
-    // console.log("inside middleware");
-    //this function acts like a middleware
-
-    // next()
-    //next passed in arguements is a fucntion which allows the enxt middleware to be executed
-
-    res.send("<h1>using express here </h1>");
-});
-
 app.listen(4000);
 // const server = http.createServer(app);
 // server.listen(4000);
