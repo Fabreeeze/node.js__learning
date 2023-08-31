1d83387f6e855063b0e06cdac83904f3a4efb9cb
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-08-31 23:57:50 +0530

    commit five, added html files to send over routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,17 @@
-const express =  require('express');
+const path = require('path')
+const express =  require('express')
 
 const router= express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.send("<h1>Using express.js here </h1>");
+    res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    //we use __dirname as we need absolute path of our projects address on system
+    //it takes absolute path of current file in system, then we go up an level and then 
+    // next two arguements specify repsctive folder and file
+     
+    //path.join adjusts the slashes used '/'(linux) or '\'(windows) based on operating system automatically
 });
+
 
 
 module.exports = router;
